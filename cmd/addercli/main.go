c02ca5feb4e753603fc5d7746aacf1a890b5f6d6
Martin Czygan <martin.czygan@gmail.com>
2023-04-14 12:16:28 +0200

    add colors

--- cmd/addercli/main.go
+++ cmd/addercli/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
+	"github.com/fatih/color"
 	"github.com/miku/adder"
 )
 
+var (
+	a = flag.Int("a", 0, "some number, a")
+	b = flag.Int("b", 0, "another number, b")
+)
+
 func main() {
-	fmt.Println(adder.Add(1, 2))
+	flag.Parse()
+	color.Cyan("Welcome to Adder!")
+	fmt.Println(adder.Add(*a, *b))
 }
